9a8cebdb3832d727fb114434a74d494676e0f93e
Alexa <Alexa@DESKTOP-KBHJ2FD>
2024-09-18 22:14:36 -0600

    Agregar archivos de proyecto.

---
+++ LibroAPI/Models/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibroAPI.Models
+{
+    public class Libro
+    {
+
+      
+        public int Id { get; set; }
+     
+        public string Titulo { get; set; }
+
+        public string? Auto { get; set; }
+
+
+        public DateTime FechaPublicacion{ get; set; }
+    }
+}
---
+++ LibroAPI/Models/LibroContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibroAPI.Models
+{
+    public class LibroContext : DbContext
+    {
+        public LibroContext(DbContextOptions<LibroContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Libro> LibroSet { get; set; }
+
+    }
+}
